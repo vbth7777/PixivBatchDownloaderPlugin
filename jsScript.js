253637c772bcb36960cdd18c2513251e18c7e93a
vbth7777 <vbth7777@gmail.com>
2023-03-31 18:30:59 +0700

    31/03/2023

--- jsScript.js
+++ jsScript.js
@@ -214,7 +214,22 @@ var Token = /** @class */ (function () {
     return Token;
 })();
 const token = new Token();
-
+if (!("path" in Event.prototype))
+Object.defineProperty(Event.prototype, "path", {
+  get: function() {
+    var path = [];
+    var currentElem = this.target;
+    while (currentElem) {
+      path.push(currentElem);
+      currentElem = currentElem.parentElement;
+    }
+    if (path.indexOf(window) === -1 && path.indexOf(document) === -1)
+      path.push(document);
+    if (path.indexOf(window) === -1)
+      path.push(window);
+    return path;
+  }
+});
 let isAppend = false;
 isOutBox = false;
 isGhimBox = false;
